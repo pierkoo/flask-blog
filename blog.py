650aa3caeb2a8a84733ee6b795507a254c1650b3
pierkoo <pierkoo@gmail.com>
2020-02-20 22:59:31 +0100

    added templates & user autenthication

--- blog.py
+++ blog.py
@@ -10,10 +10,14 @@ from flask import redirect
 from flask import url_for
 from flask import g
 import sqlite3
+from functools import wraps
 
 # Configuration
 
 DATABASE = "blog.db"
+USERNAME = "admin"
+PASSWORD = "admin"
+SECRET_KEY = "hard_to_guess"
 
 app = Flask(__name__)
 
@@ -23,6 +27,43 @@ app.config.from_object(__name__)
 # Function used for connecting to the database
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+def login_required(test):
+    @wraps(test)
+    def wrap(*args, **kwargs):
+        if "logged_in" in session:
+            return test(*args, **kwargs)
+        else:
+            flash("You need to log in first.")
+            return redirect(url_for("login"))
+    return wrap
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    error = None
+    status_code = 200
+    if request.method == "POST":
+        if request.form["username"] != app.config["USERNAME"] or \
+                request.form["password"] != app.config["PASSWORD"]:
+            error = " Invalid Credentials. Please try again."
+            status_code = 401
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+
+    return render_template("login.html", error=error), status_code
+
+@app.route("/main")
+@login_required
+def main():
+    return render_template("main.html")
+
+@app.route("/logout")
+def logout():
+    session.pop("logged_in", None)
+    flash("You were logged out")
+    return redirect(url_for("login"))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
